a50036c3da0bcfe3349852be37a2b19dedb24612
DoubleDiamond <tvon777@gmail.com>
2016-04-27 19:51:35 -0400

    boop

--- TestingPackage/MyString.py
+++ TestingPackage/MyString.py
@@ -12,51 +12,74 @@ class MyString():
     #Only letters a, e, i, o, and u (both lower and upper case) are considered vowels.
     #The returned string contains each occurrence of a vowel in the current string.
     def getVowels(self):
-        lowercase=self.str
-        lowercase=lowercase.lower()
+        lowercase = self.str
+        lowercase = lowercase.lower()
         vowels=""
-        count=len(lowercase)
+        count = len(lowercase)
 
         for a in range(0,count):
             if lowercase[a]=='a'or lowercase[a]=='e' or lowercase[a]== 'i' or lowercase[a]=='o' or lowercase[a]== 'u':
-                vowels=vowels+self.str[a]
+                vowels = vowels+self.str[a]
         return vowels
 
     #Returns a string that consists of the substring between start and end indexes (both included) in the current string.
     #Index 1 corresponds to the first character in the current string.
-    def getSubstring( self, start, end):
-        self.startswith = start
-        self.endswith = end
-        character = ""
-        count = len(character)
-        count = len.s
-        for i in range(1, count):
-            while character:
+    def getSubstring(self, start, end):
+        sub = ""
+        try:
+            for a in range(start-1, end):
+                sub = sub+self.str[a]
+        except IndexError:
+            return("Index out of bounds.")
+        return sub
 
 
 
-        return 1
+        #self.startswith = start
+        #self.endswith = end
+        #character = ""
+        #count = len(character)
+        #count = len.s
+        #for i in range(1, count):
+          #  while character:
+
+
+
+        #return 1
     #Breaks the string down, and returns it as a character string
     def getCharList(self):
-
-        return 1
+        charlist = []
+        counter = len(self.str)
+        print(counter)
+        for a in range(0, counter):
+            charlist.append(self.str[a])
+        return charlist
 
     #Returns the index of the first occurrence of a character in the current string.
     #Index 1 corresponds to the first character in the current string.
     # return 0 if no match is found
     def indexOf(self,c):
+        counter = len(self.str) - 1
+        a = 0
+        while(a < counter):
+            if self.str[a] == c:
+                return a + 1
+            a += 1
 
         return 0
 
 
     # Removes all occurrences of the specified character from the current string.
     def removeChar(self,c):
+        string = self.str
 
-        return 1
+
+        #strike = self.str
+        #self.str = self.replace(c,'')
+        #return strike
 
 
     #Invert the current string.
     def invert(self):
-
-
-        return 1
+        reverse = self.str[::-1] #list slices ftw
+        return reverse
